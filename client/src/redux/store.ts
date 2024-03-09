716ce31461979e4ce7f1006d5bb3b119f9951877
Saadkhan Pathan <saadkhanp@cybage.com>
2024-03-09 22:57:52 +0530

    SM-2 updated interfaces slices and pages

--- client/src/redux/store.ts
+++ client/src/redux/store.ts
@@ -1,42 +1,39 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import authReducer from "./features/authSlice";
-// import userReducer from "./features/userSlice";
-// import postReducer from "./features/postSlice";
-// import commentReducer from "./features/commentSlice";
-// import suggestionUserReducer from "./features/suggestionUserSlice";
-// import notificationReducer from "./features/notificationSlice";
-// import conversationReducer from "./features/conversationSlice";
-// import messagesReducer from "./features/messagesSlice";
-// import OnlineReducer from "./features/onlineSlice";
-// import CallReducer from "./features/callSlice";
+import userReducer from "./features/userSlice";
+import postReducer from "./features/postSlice";
+import commentReducer from "./features/commentSlice";
+import suggestionUserReducer from "./features/suggestionUserSlice";
+import notificationReducer from "./features/notificationSlice";
+import conversationReducer from "./features/conversationSlice";
+import messagesReducer from "./features/messagesSlice";
+import OnlineReducer from "./features/onlineSlice";
 
-// import uploadImgReducer from "./features/uploadImgSlice";
 
-// import GlobalStateSlice from "./features/GlobalStateSlice";
-// import SocketSlice from "./features/socketSlice";
-// import PeerSlice from "./features/peerSlice";
-import infoFacebookSlice from "./features/infoFacebookSlice";
+import uploadImgReducer from "./features/uploadImgSlice";
 
+import GlobalStateSlice from "./features/GlobalStateSlice";
+import SocketSlice from "./features/socketSlice";
+import PeerSlice from "./features/peerSlice";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
-    // user: userReducer,
-    // upload: uploadImgReducer,
-    // post: postReducer,
-    // comment: commentReducer,
-    // suggestionUser: suggestionUserReducer,
-    // notification: notificationReducer,
-    // conversation: conversationReducer,
-    // messages: messagesReducer,
-    // online: OnlineReducer,
-    // call: CallReducer,
-    infoFaceBook: infoFacebookSlice,
+    user: userReducer,
+    upload: uploadImgReducer,
+    post: postReducer,
+    comment: commentReducer,
+    suggestionUser: suggestionUserReducer,
+    notification: notificationReducer,
+    conversation: conversationReducer,
+    messages: messagesReducer,
+    online: OnlineReducer,
+    
 
-    // socket: SocketSlice,
-    // peer: PeerSlice,
-    // globalState: GlobalStateSlice,
+    socket: SocketSlice,
+    peer: PeerSlice,
+    globalState: GlobalStateSlice,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
