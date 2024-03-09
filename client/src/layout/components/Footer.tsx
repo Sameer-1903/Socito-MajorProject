8cca04500731a1c3b711bd77ac2d6e5d2f4c5615
Saadkhan Pathan <saadkhanp@cybage.com>
2024-03-09 22:54:30 +0530

    updted interfaces slices and pages

---
+++ client/src/layout/components/Footer.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Footer: React.FC = () => {
+  return (
+    <footer>
+      <div className="container">
+        
+        <div className="copyright-notice container-fluid">
+          © 2024 Socito 
+          
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
