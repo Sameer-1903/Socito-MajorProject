a373b5880bc62237a4a7f30b59fb17b4a69bb0d8
Saadkhan Hamidkhan Pathan <153599497+saadkhanp@users.noreply.github.com>
2024-03-11 14:31:29 +0530

    Update peerSlice.ts

--- client/src/redux/features/peerSlice.ts
+++ client/src/redux/features/peerSlice.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { createSlice } from "@reduxjs/toolkit";
 import { PeerState } from "../../utils/interface";
 
@@ -19,25 +18,3 @@ const socketSlice = createSlice({
 export const { setPeer } = socketSlice.actions;
 
 export default socketSlice.reducer;
-=======
-import { createSlice } from "@reduxjs/toolkit";
-import { PeerState } from "../../utils/interface";
-
-const initialState: PeerState = {
-  data: null,
-};
-
-const socketSlice = createSlice({
-  name: "peer",
-  initialState: initialState,
-  reducers: {
-    setPeer: (state, action) => {
-      state.data = action.payload;
-    },
-  },
-});
-
-export const { setPeer } = socketSlice.actions;
-
-export default socketSlice.reducer;
->>>>>>> SM-5-post-details-page
