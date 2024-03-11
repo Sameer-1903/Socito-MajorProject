33c00a97a86bbf9ac581c3d4ee4d0b6a112ea1af
Saadkhan Hamidkhan Pathan <153599497+saadkhanp@users.noreply.github.com>
2024-03-11 14:40:36 +0530

    Update conversationServices.ts

--- client/src/services/conversationServices.ts
+++ client/src/services/conversationServices.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import axios from "axios";
 import { config } from "../utils/axiosConfig";
 
@@ -42,48 +41,3 @@ const conversationService = {
   isReadConversation,
 };
 export default conversationService;
-=======
-import axios from "axios";
-import { config } from "../utils/axiosConfig";
-
-import { BASE_URL } from "../utils/baseUrl";
-
-const createConversation = async (id: string) => {
-  const response = await axios.post(
-    `${BASE_URL}/conversation/${id}`,
-    null,
-    config()
-  );
-  return response.data;
-};
-
-const getConversations = async () => {
-  const response = await axios.get(`${BASE_URL}/conversation`, config());
-  return response.data;
-};
-
-const deleteConversation = async (id: string) => {
-  const response = await axios.delete(
-    `${BASE_URL}/conversation/delete/${id}`,
-
-    config()
-  );
-  return response.data;
-};
-const isReadConversation = async (id: string) => {
-  const response = await axios.put(
-    `${BASE_URL}/conversation/${id}`,
-    null,
-    config()
-  );
-  return response.data;
-};
-
-const conversationService = {
-  createConversation,
-  getConversations,
-  deleteConversation,
-  isReadConversation,
-};
-export default conversationService;
->>>>>>> SM-5-post-details-page
