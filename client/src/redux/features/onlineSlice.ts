8e42c9c81c5c2d632f9564d31c3d54d7d3cd89a2
Saadkhan Hamidkhan Pathan <153599497+saadkhanp@users.noreply.github.com>
2024-03-11 14:30:54 +0530

    Update onlineSlice.ts

--- client/src/redux/features/onlineSlice.ts
+++ client/src/redux/features/onlineSlice.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { PayloadAction, createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { OnlineState } from "../../utils/interface";
 
@@ -56,62 +55,3 @@ const onlineSlice = createSlice({
 });
 
 export default onlineSlice.reducer;
-=======
-import { PayloadAction, createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import { OnlineState } from "../../utils/interface";
-
-const initialState: OnlineState = {
-  data: [],
-};
-
-export const setOnlineToClient = createAsyncThunk(
-  "online/set-online-to-client",
-  async (id: string) => {
-    return id;
-  }
-);
-export const setOfflineToClient = createAsyncThunk(
-  "online/set-offline-to-client",
-  async (id: string) => {
-    return id;
-  }
-);
-
-export const setOnline = createAsyncThunk(
-  "online/set-online",
-  async (id: string[]) => {
-    return id;
-  }
-);
-
-const onlineSlice = createSlice({
-  name: "online",
-  initialState: initialState,
-  reducers: {},
-  extraReducers: (builder) => {
-    builder
-      .addCase(
-        setOnlineToClient.fulfilled,
-        (state, action: PayloadAction<string>) => {
-          state.data = state.data.includes(action.payload)
-            ? state.data
-            : [...state.data, action.payload];
-        }
-      )
-      .addCase(
-        setOfflineToClient.fulfilled,
-        (state, action: PayloadAction<string>) => {
-          state.data = state.data.filter((obj) => obj !== action.payload);
-        }
-      )
-      .addCase(
-        setOnline.fulfilled,
-        (state, action: PayloadAction<string[]>) => {
-          state.data = action.payload;
-        }
-      );
-  },
-});
-
-export default onlineSlice.reducer;
->>>>>>> SM-5-post-details-page
