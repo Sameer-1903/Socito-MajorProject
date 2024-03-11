f53333683eea38628016b35cc5d52b21690f0d0d
Sameer-1903 <153594755+Sameer-1903@users.noreply.github.com>
2024-03-11 14:16:54 +0530

    Update Home.tsx

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -4,23 +4,13 @@ import Helmet from "../components/Helmet";
 import PostList from "../components/home/PostList";
 import SuggestionUser from "../components/home/SuggestionUser";
 
-<<<<<<< HEAD
-import Story from "../components/Story";
-=======
 
->>>>>>> SM-5-post-details-page
 
 const Home: React.FC = () => {
   return (
     <Helmet title="Socito">
       <div className="home-wrapper">
         <div>
-<<<<<<< HEAD
-          {/* <div className="story-post-wrapper">
-            <Story />
-          </div> */}
-=======
->>>>>>> SM-5-post-details-page
           <div className="post-wrapper">
             <PostList />
           </div>
