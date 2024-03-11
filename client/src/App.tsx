9608ae80f6263caad1830abbeb6603ed932d9bab
Saadkhan Hamidkhan Pathan <153599497+saadkhanp@users.noreply.github.com>
2024-03-11 15:05:53 +0530

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,64 +1,3 @@
-<<<<<<< HEAD
-import {lazy, Suspense, useEffect} from "react"
-
-import {BrowserRouter, Routes, Route} from "react-router-dom"
-
-import Loading from "./components/Loading"
-import AuthLayout from "./layout/AuthLayout"
-import MainLayout from "./layout/MainLayout"
-
-import PrivateRouter from "./PrivateRoute"
-
-function App(){
-  return(
-    return (
-      <>
-        <BrowserRouter>
-          <Suspense fallback={<Loading />}>
-            <Routes>
-              <Route path="/" element={<AuthLayout />}>
-                <Route path="/signup" element={<Register />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/forgot-password" element={<ForgotPassWord />} />
-                <Route path="/email-sent" element={<EmailSent />} />
-                <Route
-                  path="/reset-password/:token"
-                  element={<ResetPassword />}
-                />
-  
-                <Route
-                  path="/signup/:faceBookId"
-                  element={<RegisterFaceBook />}
-                />
-              </Route>
-              <Route path="/" element={<MainLayout />}>
-                <Route
-                  index
-                  element={
-                    <PrivateRouter>
-                      <Home />
-                    </PrivateRouter>
-                  }
-                />
-  
-                <Route path="/:username" element={<Profile />} />
-  
-                <Route path="/explore" element={<Explore />} />
-  
-                <Route path="/direct/inbox" element={<MessagesDirect />} />
-                <Route path="/direct/:id" element={<Messages />} />
-  
-                <Route path="/:username/:id" element={<Post />} />
-              </Route>
-            </Routes>
-          </Suspense>
-        </BrowserRouter>
-      </>
-  )
-}
-
-export default App;
-=======
 import { lazy, Suspense, useEffect } from "react";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -99,7 +38,6 @@ function App() {
     }
   }, [dispatch, message]);
   return (
-    <>
       <BrowserRouter>
         <Suspense fallback={<Loading />}>
           <Routes>
@@ -129,9 +67,7 @@ function App() {
           </Routes>
         </Suspense>
       </BrowserRouter>
-    </>
   );
 }
 
 export default App;
->>>>>>> SM-5-post-details-page
