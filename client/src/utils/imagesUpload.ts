bb7328e4306c6eb70d21655b9ca6be625af69da1
Saadkhan Hamidkhan Pathan <153599497+saadkhanp@users.noreply.github.com>
2024-03-11 15:04:07 +0530

    Update imagesUpload.ts

--- client/src/utils/imagesUpload.ts
+++ client/src/utils/imagesUpload.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import axios from "axios";
 export const checkImage = (file: File) => {
   const types = ["image/png", "image/jpeg"];
@@ -27,33 +26,3 @@ export const imageUpload = async (file: File) => {
   const data = response.data;
   return { public_id: data.public_id, url: data.secure_url };
 };
-=======
-import axios from "axios";
-export const checkImage = (file: File) => {
-  const types = ["image/png", "image/jpeg"];
-  let err = "";
-  if (!file) return (err = "File does not exist.");
-
-  if (file.size > 1024 * 1024)
-    // 1mb
-    err = "The largest image size is 1mb";
-
-  if (!types.includes(file.type)) err = "The image type is png / jpeg";
-
-  return err;
-};
-
-export const imageUpload = async (file: File) => {
-  const formData = new FormData();
-  formData.append("file", file);
-  formData.append("upload_preset", "st6cap4v");
-  formData.append("cloud_name", "dt36g5k7q");
-
-  const response = await axios.post(
-    "https://api.cloudinary.com/dt36g5k7q/upload"
-  );
-
-  const data = response.data;
-  return { public_id: data.public_id, url: data.secure_url };
-};
->>>>>>> SM-5-post-details-page
